64ff7acaf27bfc8b474109d6168ea7b71495ff27
Alex <alexeym75@gmail.com>
2022-07-11 23:47:08 +0700

    Tasks 4,5

--- src/Main.java
+++ src/Main.java
@@ -31,5 +31,37 @@ public class Main {
         int breakfastGramms = (5 * banana) + (2 * milk) + (2 * icecream) + (4 * egg);
         System.out.println("Вес спорт-завтрака: " + breakfastGramms + " грамм");
         System.out.println("Вес спорт-завтрака: " + breakfastGramms / 1000 + " кг. и " + breakfastGramms % 1000 + " грамм");
+
+        // Task 4
+        byte days250 = 7000 / 250;
+        byte days500 = 7000 / 500;
+
+        System.out.println("Если худеть по 250 г. в день, то 7 кг. можно сбросить за " + days250 + " дней.");
+        System.out.println("Если худеть по 500 г. в день, то 7 кг. можно сбросить за " + days500 + " дней.");
+        System.out.println("В среднем похудеть можно за " + (days250 + days500) / 2 + " дней.");
+
+        // Task 5
+        int salaryMasha = 67760;
+        int salaryDenis = 83690;
+        int salaryKristina = 76230;
+
+        int salaryMashaInc = salaryMasha + salaryMasha / 10;
+        int salaryDenisInc = salaryDenis + salaryDenis / 10;
+        int salaryKristinaInc = salaryKristina + salaryKristina / 10;
+
+        int salaryMashaYear = salaryMasha * 12;
+        int salaryDenisYear = salaryDenis * 12;
+        int salaryKristinaYear = salaryKristina * 12;
+
+        int salaryMashaYearInc = salaryMashaInc * 12;
+        int salaryDenisYearInc = salaryDenisInc * 12;
+        int salaryKristinaYearInc = salaryKristinaInc * 12;
+
+        System.out.println("Маша теперь получает " + salaryMashaInc + " рублей/месяц. " +
+                "Годовой доход вырос на " + (salaryMashaYearInc - salaryMashaYear) + " рублей.");
+        System.out.println("Денис теперь получает " + salaryDenisInc + " рублей/месяц. " +
+                "Годовой доход вырос на " + (salaryDenisYearInc - salaryDenisYear) + " рублей.");
+        System.out.println("Кристина теперь получает " + salaryKristinaInc + " рублей/месяц. " +
+                "Годовой доход вырос на " + (salaryKristinaYearInc - salaryKristinaYear) + " рублей.");
     }
 }
